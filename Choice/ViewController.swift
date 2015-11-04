163c62d8d3bf1f433d5df3a2f54d2a90d16dd866
Rosalyn Koscica <rkoscica@rkoscica-2016.local>
2015-11-04 15:22:03 -0500

    Initial Commit

---
+++ Choice/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Choice
+//
+//  Created by Rosalyn Koscica on 11/4/15.
+//  Copyright © 2015 Rosalyn Koscica. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
