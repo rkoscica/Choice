292d0ddc65c7354887db718a6311590674519cef
Rosalyn Koscica <rkoscica@rkoscica-2016.local>
2015-11-05 15:45:53 -0500

    Buttons work with choiceTree consequence function, reset button does not work yet

--- Choice/ViewController.swift
+++ Choice/ViewController.swift
@@ -8,18 +8,16 @@
 
 import UIKit
 
+
 class ViewController: UIViewController {
-    var displayText = Consequence(choice: "null")
     
-    @IBOutlet weak var choiceA: UIButton!
-    @IBOutlet weak var choiceB: UIButton!
-    @IBOutlet weak var choiceC: UIButton!
-    @IBOutlet weak var choiceD: UIButton!
-    @IBOutlet weak var menuButton: UIBarButtonItem!
+    @IBOutlet weak var textArea: UITextView!
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        textArea.text = consequence("start")
     }
 
     override func didReceiveMemoryWarning() {
@@ -27,10 +25,26 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    @IBAction func pickChoice () {
-        
+    
+    @IBAction func bOnePressed () {
+        //textArea.text = "You have died."
+        textArea.text = consequence("A")
     }
-
+    @IBAction func bTwoPressed () {
+        //textArea.text = "You have lived."
+        textArea.text = consequence("B")
+    }
+    @IBAction func bThreePressed () {
+        //textArea.text = "You escape with a large wound."
+        textArea.text = consequence("C")
+    }
+    @IBAction func bFourPressed () {
+        //textArea.text = "You find yourself stuck in a swamp."
+        textArea.text = consequence("D")
+    }
+    @IBAction func resetPressed(){
+        textArea.text = consequence("")
+    }
 
 }
 
--- Choice/choiceTree.swift
+++ Choice/choiceTree.swift
@@ -9,11 +9,18 @@
 import Foundation
 import UIKit
 
-class Consequence {
-    var type: String?
-    var age: Int?
-    
-    init(choice: String) {
-        
+func consequence (let choice: String) -> String {
+        var status = ""
+    if choice == "A" {
+        status = "You died."
+    } else if choice == "B" {
+        status = "You manage to survive."
+    } else if choice == "C" {
+        status = "You escape with a large wound."
+    } else if choice == "D" {
+        status = "You find yourself stuck in a swamp."
+    } else {
+        status = "You find yourself in a forest with no memory of how you came to be there. To your right, there is a cliff. To your left, there is a helicopter. Behind you, there is a hungry mountain lion. An awful smell emanates from the path in front of you. The sun is setting and you must find shelter. What do you do?"
     }
+    return status
 }
