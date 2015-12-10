92a7f7bbd7d704ab5e537a1c30ce5f956a879f4b
Rosalyn Koscica <rkoscica@rkoscica-2016.local>
2015-12-10 12:54:09 -0500

    2 scenarios, mysterious swap of option 1 between scenarios when app is run

--- Choice/choiceTree.swift
+++ Choice/choiceTree.swift
@@ -10,11 +10,28 @@ import Foundation
 import UIKit
 
 var cycle: Int = 0
-var storyArray = ["You find yourself in a forest with no memory of how you came to be there. To your right, there is a cliff. To your left, there is a helicopter. Behind you, there is a hungry mountain lion. An awful smell emanates from the path in front of you. The sun is setting and you must find shelter. What do you do?", "You died.", "You manage to survive.", "You escape with a large wound.", "You find yourself stuck in a swamp.", "Jump off the cliff.", "Try to fly the helicopter.", "Try to run past the lion.", "Hold your nose and walk forward.", "You are in a deserted shopping mall. The doors have locked, the employees have gone home, and the friends who came with you are nowhere to be seen."]
+var storyArray = [
+    "You find yourself in a forest with no memory of how you came to be there. To your right, there is a cliff. To your left, there is a helicopter. Behind you, there is a hungry mountain lion. An awful smell emanates from the path in front of you. The sun is setting and you must find shelter. What do you do?", "Jump off the cliff.", "Try to fly the helicopter.", "Try to run past the lion.", "Hold your nose and walk forward.", "You died.", "You manage to survive.", "You escape with a large wound.", "You find yourself stuck in a swamp.",
+    "You are in a deserted shopping mall. The doors have locked, the employees have gone home, and the friends who came with you are nowhere to be seen.", "Take a nap.", "Call the police.", "Try to break a window.", "Loot the shops.", "You wake up the next morning and find that the mall has opened again. However, the security guard who found you looks very unamused.", "Your phone runs out of battery before you can explain the situation.", "You successfully escape.", "After hours of searching, you have plenty of valuable trinkets. Exhaustion finally overcomes you, but at least you have plenty of new stuff... right? Unfortunately, the security cameras have caught your entire spree, and the authorities quickly confiscate all of your new possessions.",
+    "You are at home."]
 
 func consequence (let mode: String, let choice: String) -> String {
         var status = ""
     if mode == "Story" {
+        if choice == "start" {
+            status = storyArray[0]
+        } else if choice == "A" {
+            status = storyArray[cycle*9 + 5]
+        } else if choice == "B" {
+            status = storyArray[cycle*9 + 6]
+        } else if choice == "C" {
+            status = storyArray[cycle*9 + 7]
+        } else if choice == "D" {
+            status = storyArray[cycle*9 + 8]
+        } else if choice == "next" {
+            status = storyArray[cycle*9]
+        }
+    } else if mode == "Option" {
         if choice == "A" {
             status = storyArray[cycle*9 + 1]
         } else if choice == "B" {
@@ -23,23 +40,9 @@ func consequence (let mode: String, let choice: String) -> String {
             status = storyArray[cycle*9 + 3]
         } else if choice == "D" {
             status = storyArray[cycle*9 + 4]
-        } else if choice == "next" || choice == "start" {
-            status = storyArray[cycle*9]
-        }
-    } else if mode == "Option" {
-        if choice == "A" {
-            status = storyArray[cycle*9 + 5]
-        } else if choice == "B" {
-            status = storyArray[cycle*9 + 6]
-        } else if choice == "C" {
-            status = storyArray[cycle*9 + 7]
-        } else if choice == "D" {
-            status = storyArray[cycle*9 + 8]
-        } else if choice == "next" || choice == "start" {
-            status = storyArray[cycle*9]
-        }
+        } 
     }
-    if cycle*9 + 18 < storyArray.count{
+    if cycle*9 + 17 < storyArray.count{
         cycle++
     } else {
         cycle = 0
